49a9b3eb39e83aa839983e19fe5ad56f1af48470
Eduardo de Rivero <eduardomanrique@ravn.co>
2022-10-01 13:56:22 -0500

    chore: enabled cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
 
   await app.listen(configService.get<number>('port'));
